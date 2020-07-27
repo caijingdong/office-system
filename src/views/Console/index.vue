ba273b2af107f7c6076e62f69798f86e027e518d
caijingdong <1007875197@qq.com>
2020-07-27 17:28:27 +0800

    d

--- src/views/Console/index.vue
+++ src/views/Console/index.vue
@@ -1,31 +1,53 @@
 <template>
   <div id="home">
-     控制台首页
-    
+    <el-row :gutter="28">
+      <el-col :span="20">
+        <div class="label-wrap">
+          <label>关键字：</label>
+          <div class="warp-content">
+            <el-row :gutter="20">
+              <el-col :span="4">
+                <el-select v-model="region" placeholder="请选择活动区域">
+                  <el-option label="区域一" value="shanghai"></el-option>
+                  <el-option label="区域二" value="beijing"></el-option>
+                </el-select>
+              </el-col>
+              <el-col :span="4">
+                <el-input placeholder="请输入搜索的关键字"></el-input>
+              </el-col>
+              <el-col :span="4">
+                <el-button type="danger">搜索</el-button>
+              </el-col>
+            </el-row>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="8">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 <script>
 export default {
   components: {},
   data() {
-
     return {
+      region: "",
     };
   },
   created() {
     //this.getUseinfo()
   },
   computed: {},
-  methods: {
-
-  }
+  methods: {},
 };
 </script>
 <style lang="less" scoped>
-#home{
-    margin:30px;
+#home {
+  margin: 30px;
 }
-
-
-
+label {
+  float: left;
+}
 </style>
--- src/views/Info/components/Nav.vue
+++ src/views/Info/components/Nav.vue
@@ -8,26 +8,62 @@
       background-color="#1767ff"
       text-color="#fff"
       active-text-color="#fff"
+      style="margin-top:150px"
     >
       <el-submenu index="1">
         <template slot="title" style="color:white;">
           <i class="el-icon-s-help" style="color:white;"></i>
-          <span slot="title" style="color:white;">导航一</span>
+          <span slot="title" style="color:white;">执法行动</span>
         </template>
-        <el-menu-item-group>
-          <span slot="title">分组一</span>
-          <el-menu-item index="1-1" @click="goUser">首页</el-menu-item>
-          <el-menu-item index="1-2" @click="goHome">控制台</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
+        <template  style="color:white;">
+          <el-menu-item style="padding-left:48px;" @click="goUser" >公安执法活动监督表</el-menu-item>
+          <el-menu-item index="1-2" style="padding-left:48px;" @click="goHome">结案方式维护</el-menu-item>
+          <el-menu-item index="1-2" style="padding-left:48px;" @click="goHome">结案方式维护</el-menu-item>              
+        </template>
+      </el-submenu>
+      <el-submenu index="2">
+        <template slot="title" style="color:white;">
+          <i class="el-icon-bank-card" style="color:white;"></i>
+          <span slot="title" style="color:white;">司法判决</span>
+        </template>
+        <template  style="color:white;">
+          <el-menu-item style="padding-left:48px;" @click="goUser" >首页</el-menu-item>
+          <el-menu-item index="1-2" style="padding-left:48px;" @click="goHome">控制台</el-menu-item>          
+        </template>
+      </el-submenu>
+      <el-submenu index="3">
+        <template slot="title" style="color:white;">
+          <i class="el-icon-discount" style="color:white;"></i>
+          <span slot="title" style="color:white;">刑事执行</span>
+        </template>
+        <template  style="color:white;">
+          <el-menu-item style="padding-left:48px;" @click="goUser" >首页</el-menu-item>
+          <el-menu-item index="1-2" style="padding-left:48px;" @click="goHome">控制台</el-menu-item>          
+        </template>
+      </el-submenu>
+      <el-submenu index="4">
+        <template slot="title" style="color:white;">
+          <i class="el-icon-set-up" style="color:white;"></i>
+          <span slot="title" style="color:white;">民事行政</span>
+        </template>
+        <template  style="color:white;">
+          <el-menu-item style="padding-left:48px;" @click="goUser" >首页</el-menu-item>
+          <el-menu-item index="1-2" style="padding-left:48px;" @click="goHome">控制台</el-menu-item>          
+        </template>
+      </el-submenu>
+            <el-submenu index="5">
+        <template slot="title" style="color:white;">
+          <i class="el-icon-s-help" style="color:white;"></i>
+          <span slot="title" style="color:white;">监外执行</span>
+        </template>
+        <template  style="color:white;">
+          <el-menu-item style="padding-left:48px;" @click="goUser" >首页</el-menu-item>
+          <el-menu-item index="1-2" style="padding-left:48px;" @click="goHome">控制台</el-menu-item>          
+        </template>
       </el-submenu>
 
-      <el-menu-item index="4">
-        <i class="el-icon-s-help" style="color:white;"></i>
-        <span slot="title">导航四</span>
-      </el-menu-item>
+
+
     </el-menu>
   </div>
 </template>
@@ -50,12 +86,11 @@ export default {
     handleClose(key, keyPath) {
       console.log(key, keyPath);
     },
-    goUser(){
-      this.$router.push({path:'/user'})
+    goUser() {
+      this.$router.push({ path: "/user" });
     },
-    goHome(){
-      this.$router.push({path:'/index'})
-
+    goHome() {
+      this.$router.push({ path: "/index" });
     }
   }
 };
@@ -68,6 +103,9 @@ export default {
   top: 0;
   left: 0;
   height: 100vh;
+}
+.item-list{
+  padding-left:48px;
 }
 /* .el-menu-vertical-demo:not(.el-menu--collapse) {
     width: 200px;
