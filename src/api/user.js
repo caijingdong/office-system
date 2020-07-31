04e6796a6bcfc9a738335baaf2df9bbbb426d93e
caijingdong <1007875197@qq.com>
2020-07-31 17:07:43 +0800

    dad

--- src/api/user.js
+++ src/api/user.js
@@ -3,11 +3,11 @@ import service from "@/utils/http";
 /**
  * 获取验证码
  */
-export function GetSms(data){
+export function GetList(){
     return service.request({
         method: "post",
-        url: "/getSms/",
-        data
+        url: "/news/getList/",
+        
         // data: data, 左边的data是变量名（key）后台接收的。右边的Data是接收的参数。如果两者都是同名的情况下，可以写成单一个即可（ES6的写法）
     })
 }
--- src/components/Table/index.vue
+++ src/components/Table/index.vue
@@ -2,15 +2,16 @@
   <div style="width: 100%;margin-top:20px;">
     <el-table :data="tableData">
       <!--多选框-->
+        <el-table-column v-if="selection" type="selection" width="50">
+        </el-table-column>
       <template v-for="item in config.tHead">
         <!--v-slot-->
+
         <el-table-column        
           :key="item.field"
           :prop="item.field"
           :label="item.label"
-          :width="item.width"
-          
-          
+          :width="item.width"          
         >
         </el-table-column>
 
@@ -19,6 +20,8 @@
   </div>
 </template>
 <script>
+import { GetList } from "@/api/user";
+import { AddFristCategory, GetCategory, DeleteCategory, EditCategory, AddChildrenCategory } from "@/api/news";
 export default {
   name: "tableVue",
   props: {
@@ -46,8 +49,31 @@ export default {
           phone: '上海',
           address: '上海市普陀区金沙江路 1518 弄',
           role: 200333
-        }]
+        }],
+        selection:false,
+        categoryName:"1111"
     };
+  },
+   methods: {
+     getDetaillist(){
+        let requestData = {
+                pageNumber:1,
+                pageSize:1
+            };
+/*        GetList(requestData)
+            .then(res => {
+          //this.countDown(number)
+        })
+        .catch(() => {}); */
+        AddFristCategory({ categoryName: this.categoryName }).then(res => {
+
+        })
+     }
+
+  },
+  created() {
+    this.getDetaillist();
+
   },
 };
 </script>
